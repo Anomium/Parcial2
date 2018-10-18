9385370f1ee92a80d875f67bd4615f4c5f7674a5
Anomium <melvin.martinez.97@gmail.com>
2018-10-17 20:34:28 -0500

    Terminado

--- src/Controller/ContainerController.java
+++ src/Controller/ContainerController.java
@@ -1,29 +1,172 @@
 package Controller;
+
 import Model.Container;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Date;
 import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class ContainerController {
-    private Queue<Container> cola = new Queue<Container>();
 
-    /*public void create(Container n1) {
-        container.push(n1);
+    Date fecha = new Date(Calendar.getInstance().getTimeInMillis());
+    SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yy");
+    
+    Stack<Container> pila = new Stack<Container>();
+    Queue<Container> standby = new LinkedList();
+    Queue<Container> inspeccion = new LinkedList();
+    Queue<Container> RevisionLicencia = new LinkedList();
+    Queue<Container> Importacion = new LinkedList();
+    List<Container> Nacionalizada = new ArrayList();
+    
+    
+
+    public void create(Container n1) {
+        pila.push(n1);
     }
-
-    public ArrayList<String[]> Read(String filter) {
-        ArrayList<String[]> Get = new ArrayList<>();
-        for (int i = 0; i < container.size(); i++) {
-            Get.add(new String[]{});
-        }
+    
+    public void Salida(){
+        standby.offer(new Container(
+            pila.get(pila.size()-1).getCodigo(),
+            pila.get(pila.size()-1).getNombre(),
+            pila.get(pila.size()-1).getPeso(),
+            pila.get(pila.size()-1).getAltura(),
+            pila.get(pila.size()-1).getColor(),
+            Fecha(),
+            pila.get(pila.size()-1).getFInspeccFisica(),
+            pila.get(pila.size()-1).getFrevisionLic(),
+            pila.get(pila.size()-1).getFimportacion()
+        ));
+        pila.pop();
+    }
+    
+    public void inspeccion(){
+        inspeccion.offer(new Container(
+            standby.peek().getCodigo(),
+            standby.peek().getNombre(),
+            standby.peek().getPeso(),
+            standby.peek().getAltura(),
+            standby.peek().getColor(),
+            standby.peek().getFSalidaCont(),
+            Fecha(),
+            standby.peek().getFrevisionLic(),
+            standby.peek().getFimportacion()
+        ));
+        standby.poll();
+    }
+    
+    public void RevisionLicencia(){
+        RevisionLicencia.offer(new Container(
+            inspeccion.peek().getCodigo(),
+            inspeccion.peek().getNombre(),
+            inspeccion.peek().getPeso(),
+            inspeccion.peek().getAltura(),
+            inspeccion.peek().getColor(),
+            inspeccion.peek().getFSalidaCont(),
+            inspeccion.peek().getFInspeccFisica(),
+            Fecha(),
+            inspeccion.peek().getFimportacion()
+        ));
+        inspeccion.poll();
+    }
+    
+    public void Nacionalizada(){
+        Nacionalizada.add(new Container(
+            RevisionLicencia.peek().getCodigo(),
+            RevisionLicencia.peek().getNombre(),
+            RevisionLicencia.peek().getPeso(),
+            RevisionLicencia.peek().getAltura(),
+            RevisionLicencia.peek().getColor(),
+            RevisionLicencia.peek().getFSalidaCont(),
+            RevisionLicencia.peek().getFInspeccFisica(),
+            RevisionLicencia.peek().getFrevisionLic(),
+            Fecha()
+        ));
+        RevisionLicencia.poll();
+    }
+    
+    public Stack<String[]> ReadMercancia() {
+        Stack<String[]> Get = new Stack();
+        pila.forEach((item) -> {
+            Get.add(new String[]{
+                item.getCodigo(),
+                item.getNombre(),
+                item.getPeso(),
+                item.getAltura(),
+                item.getColor()
+            });
+        });
         return Get;
     }
-
-
-    public void Update(int index, Container n1) {
-        container.set(index, n1);
+    
+    public ArrayList<String[]> ReadInspeccion(){
+        ArrayList<String[]> get = new ArrayList();
+        standby.forEach((item) ->{
+            get.add(new String[]{
+                item.getCodigo(),
+                item.getNombre(),
+                item.getFSalidaCont(),
+                item.getFInspeccFisica(),
+                item.getFrevisionLic(),
+                item.getFimportacion()
+            });
+        });
+        inspeccion.forEach((item) ->{
+            get.add(new String[]{
+                item.getCodigo(),
+                item.getNombre(),
+                item.getFSalidaCont(),
+                item.getFInspeccFisica(),
+                item.getFrevisionLic(),
+                item.getFimportacion()
+            });
+        });
+        RevisionLicencia.forEach((item) ->{
+            get.add(new String[]{
+                item.getCodigo(),
+                item.getNombre(),
+                item.getFSalidaCont(),
+                item.getFInspeccFisica(),
+                item.getFrevisionLic(),
+                item.getFimportacion()
+            });
+        });
+        Importacion.forEach((item) ->{
+            get.add(new String[]{
+                item.getCodigo(),
+                item.getNombre(),
+                item.getFSalidaCont(),
+                item.getFInspeccFisica(),
+                item.getFrevisionLic(),
+                item.getFimportacion()
+            });
+        });
+        Nacionalizada.forEach((item) ->{
+            get.add(new String[]{
+                item.getCodigo(),
+                item.getNombre(),
+                item.getFSalidaCont(),
+                item.getFInspeccFisica(),
+                item.getFrevisionLic(),
+                item.getFimportacion()
+            });
+        });
+        
+        return get;
     }
 
     public void Delete() {
-        container.pop();
-    }*/
+        pila.pop();
+    }
 
+    //Metodo para la fecha
+    public String Fecha() {
+        String fechaTexto;
+        return fechaTexto = formato.format(fecha);
+    }
+      
+    
 }
--- src/Model/Container.java
+++ src/Model/Container.java
@@ -1,17 +1,60 @@
 package Model;
 public class Container {
-    private String Codigo;
+
+
+
+    private String Codigo ;
     private String Nombre;
     private String Peso;
     private String Altura;
     private String Color;
+    private String FSalidaCont;
+    private String FInspeccFisica;
+    private String FrevisionLic;
+    private String Fimportacion;
 
-    public Container(String Codigo, String Nombre, String Peso, String Altura, String Color) {
+    public Container(String Codigo, String Nombre, String Peso, String Altura, String Color, String FSalidaCont, String FInspeccFisica, String FrevisionLic, String Fimportacion) {
         this.Codigo = Codigo;
         this.Nombre = Nombre;
         this.Peso = Peso;
         this.Altura = Altura;
         this.Color = Color;
+        this.FSalidaCont = FSalidaCont;
+        this.FInspeccFisica = FInspeccFisica;
+        this.FrevisionLic = FrevisionLic;
+        this.Fimportacion = Fimportacion;
+    }
+
+    public String getFSalidaCont() {
+        return FSalidaCont;
+    }
+
+    public void setFSalidaCont(String FSalidaCont) {
+        this.FSalidaCont = FSalidaCont;
+    }
+
+    public String getFInspeccFisica() {
+        return FInspeccFisica;
+    }
+
+    public void setFInspeccFisica(String FInspeccFisica) {
+        this.FInspeccFisica = FInspeccFisica;
+    }
+
+    public String getFrevisionLic() {
+        return FrevisionLic;
+    }
+
+    public void setFrevisionLic(String FrevisionLic) {
+        this.FrevisionLic = FrevisionLic;
+    }
+
+    public String getFimportacion() {
+        return Fimportacion;
+    }
+
+    public void setFimportacion(String Fimportacion) {
+        this.Fimportacion = Fimportacion;
     }
 
     public String getCodigo() {
@@ -53,6 +96,5 @@ public class Container {
     public void setColor(String Color) {
         this.Color = Color;
     }
-    
-    
+
 }
